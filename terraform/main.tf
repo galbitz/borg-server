6fc80ec275f8c294d2b1213a87bafe349fb03707
Gabor A <galbitz@gmail.com>
2022-02-24 23:00:41 -0500

    initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+      linode = {
+        source = "linode/linode"
+        version = "1.16.0"
+      }
+  }
+}
+
+# Linode Provider definition
+
+provider  "linode" {
+  token = var.token
+}
+
+
+resource  "linode_instance"  "terraform" {
+  image = "linode/ubuntu20.04"
+  label = "webserver"
+  region = "us-east"
+  type = "g6-nanode-1"
+  authorized_keys = [ var.authorized_keys ]
+  root_pass = var.root_pass
+  backups_enabled = true
+}
